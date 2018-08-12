4aef12fa09566fec8eaa4f0a497b1eaf02a61174
SaifulBUBT <s.saifulbubt@gmail.com>
2018-08-12 11:21:16 +0600

    create an object and traverse all attributes

---
+++ lecture1.js
@@ -0,0 +1,32 @@
+var book = {
+	name: 'Functional Javascript',
+	author: 'Micbeal Fogus',
+	publisher: 'O\'Reilly',
+	page: 250,
+	print: function (){
+		console.log(this.publisher, this.page);
+	}
+
+}
+
+console.log(book.name); //obejct call using .notation
+console.log(book['author']) // object call using bracket [] notation
+console.log(book.print())
+
+/// insert new attribute in object
+console.log('peice '+book.price);
+book.price = 300;
+console.log('price '+book.price);
+
+book.publishedYear = 2017;
+console.log('publishedYear =',book.publishedYear);
+
+console.log(book)
+
+
+//// traverse all attibutes from object
+
+for(var properties in book){
+	console.log(properties +' = '+ book[properties])
+}
+
